a888558e2b7f52fb88e8008e5af2eb0a9f70ba97
Arthur Vilmar Rita <arthur1590@hotmail.com>
2019-11-27 00:41:14 -0300

    refectoring EnvironmentGraphQLType class.

--- CentralDeErros/CentralDeErros.Api/GraphQL/Types/ErrorLogType.cs
+++ CentralDeErros/CentralDeErros.Api/GraphQL/Types/ErrorLogType.cs
@@ -1,23 +1,8 @@
 ﻿using CentralDeErros.Application.ViewModel;
-using CentralDeErros.CrossCutting.CustomTypes;
 using GraphQL.Types;
 
 namespace CentralDeErros.Api.GraphQL.Types
 {
-    public class EnvironmentGraphQLType : EnumerationGraphType
-    {
-        public EnvironmentGraphQLType()
-        {
-            Name = "Environment";
-            Description = "Environment where the application is running.";
-            AddValue("Unknown", "Unknown", 0);
-            AddValue("Development", "Development", 1);
-            AddValue("Test", "Test", 2);
-            AddValue("Acceptance", "Acceptance", 3);
-            AddValue("Production", "Production", 4);
-        }
-    }
-
     public class ErrorLogType : ObjectGraphType<ErrorLogViewModel>
     {
         public ErrorLogType()
