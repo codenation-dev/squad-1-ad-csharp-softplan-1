162d3778ef02baa7c6d8ba900f38e9808f7dde3a
Arthur Vilmar Rita <arthur1590@hotmail.com>
2019-12-11 23:49:10 -0300

    Adding more tests and scripts to check code coverage
    To run code coverage, open windows powershell and go to ..\CentralDeErros\CentralDeErros.CrossCutting\Scrypts\ and run .\run_tests_and_generate_coverage_report.ps1
    
    After, you just need to check the file CentralDeErros.CrossCutting/code_coverage/index.htm

---
+++ CentralDeErros/CentralDeErros.CrossCutting.Test/Helpers/AppSettingsTest.cs
@@ -0,0 +1,19 @@
+﻿using CentralDeErros.CrossCutting.Helpers;
+using Xunit;
+
+namespace CentralDeErros.CrossCutting.Test.Helpers
+{
+    public class AppSettingsTest
+    {
+        [Fact]
+        public void Should_Possible_Set_SecretKeyJWT_Property()
+        {
+
+            AppSettings appSettings = new AppSettings();
+            string newSecretKey = "Test_String_Key";
+            appSettings.SecretKeyJWT = newSecretKey;
+
+            Assert.Equal(newSecretKey, appSettings.SecretKeyJWT);
+        }
+    }
+}
--- CentralDeErros/CentralDeErros.CrossCutting/Helpers/AppSettings.cs
+++ CentralDeErros/CentralDeErros.CrossCutting/Helpers/AppSettings.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CentralDeErros;
-
-namespace CentralDeErros.CrossCutting.Helpers
+﻿namespace CentralDeErros.CrossCutting.Helpers
 {
     public class AppSettings
     {
-
         public string SecretKeyJWT { get; set; }
     }
 }
